b14277f4dd11d185e0c25955f6676e8e9e75639b
Brendan Manning <BrendanManning19380@gmail.com>
2018-05-05 20:51:54 -0400

    Moving to callback design

--- DOORA_Door/DOORA_Door.ino
+++ DOORA_Door/DOORA_Door.ino
@@ -13,8 +13,11 @@
   #include "SSWiFi.h"
   #include <WiFiUdp.h>
 
+  // Parse network messages as JSON
+  #include <ArduinoJson.h>
+
   // Define WiFi connection objects
-  SSWiFi wifi =  SSWiFi();
+  SSWiFi wifi = SSWiFi();
   WiFiUDP Udp;
 
   /**
@@ -81,24 +84,29 @@ void loop() {
   int packetSize = Udp.parsePacket();
   if (packetSize)
   {
+    /*
     Serial.print("Received packet of size ");
     Serial.println(packetSize);
     Serial.print("From ");
     IPAddress remoteIp = Udp.remoteIP();
     Serial.print(remoteIp);
     Serial.print(", port ");
-    Serial.println(Udp.remotePort());
+    Serial.println(Udp.remotePort());*/
 
-    // read the packet into packetBufffer
+    // Read the package into the buffer
     int len = Udp.read(packetBuffer, 255);
     if (len > 0) packetBuffer[len] = 0;
-    Serial.println("Contents:");
-    Serial.println(packetBuffer);
 
-    // send a reply, to the IP address and port that sent us the packet we received
-    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-    Udp.write(ReplyBuffer);
-    Udp.endPacket();
+    // Handle the message with the callback
+    handlePacket(packetBuffer);
+
   }
+}
+
+/**
+ * Called everytiime a valid string package is recieved
+ */
+void handlePacket(char[] packet) {
+  
 }
 
